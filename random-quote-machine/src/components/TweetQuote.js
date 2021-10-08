fe8820c4ed170ad4f7cda3f3757d6793eec8b048
sue <infosuemcmahon@gmail.com>
2021-10-08 14:47:43 +0100

    Add Twitter share icon

--- random-quote-machine/src/components/TweetQuote.js
+++ random-quote-machine/src/components/TweetQuote.js
@@ -1,8 +1,14 @@
 import './TweetQuote.css';
+import twitterLogo from '../images/twitter-share-icon.svg';
 
 const TweetQuote = ({tweetQuote}) => {
     return (
-        <a id="tweet-quote" className="button tweet-quote" target="_blank" href={`https://twitter.com/intent/tweet?text=${tweetQuote}`} rel="noopener noreferrer">Tweet Quote</a>
+        <div className="social">
+            <p>Share quote on Twitter</p>
+            <a id="tweet-quote" className="tweet-quote" target="_blank" href={`https://twitter.com/intent/tweet?text=${tweetQuote}`} rel="noopener noreferrer">
+                <img src={twitterLogo} className="App-logo" alt="Twitter logo" width="50" height="50" title="Share quote on Twitter" />
+            </a>
+        </div>
     );
 }
 export default TweetQuote;
