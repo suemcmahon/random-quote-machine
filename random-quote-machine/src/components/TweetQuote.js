adefbb7889d1f4212fae5d0a7745636d9e652c0a
sue <infosuemcmahon@gmail.com>
2021-10-08 16:01:12 +0100

    Update font and colours

--- random-quote-machine/src/components/TweetQuote.js
+++ random-quote-machine/src/components/TweetQuote.js
@@ -3,12 +3,10 @@ import twitterLogo from '../images/twitter-share-icon.svg';
 
 const TweetQuote = ({tweetQuote}) => {
     return (
-        <div className="social">
-            <p>Share quote on Twitter</p>
-            <a id="tweet-quote" className="tweet-quote" target="_blank" href={`https://twitter.com/intent/tweet?text=${tweetQuote}`} rel="noopener noreferrer">
-                <img src={twitterLogo} className="App-logo" alt="Twitter logo" width="50" height="50" title="Share quote on Twitter" />
-            </a>
-        </div>
+        <a id="tweet-quote" className="tweet-quote" target="_blank" href={`https://twitter.com/intent/tweet?text=${tweetQuote}`} rel="noopener noreferrer">
+            <img src={twitterLogo} className="App-logo" alt="Twitter logo" width="50" height="50" title="Share quote on Twitter" />
+        </a>
+        
     );
 }
 export default TweetQuote;
