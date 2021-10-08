c2d2d55762b59d5b05adcfec585e00181858c812
sue <infosuemcmahon@gmail.com>
2021-10-08 19:34:24 +0100

    Add transistions

--- random-quote-machine/src/components/TweetQuote.js
+++ random-quote-machine/src/components/TweetQuote.js
@@ -4,7 +4,7 @@ import twitterLogo from '../images/twitter-share-icon.svg';
 const TweetQuote = ({tweetQuote}) => {
     return (
         <a id="tweet-quote" className="tweet-quote" target="_blank" href={`https://twitter.com/intent/tweet?text=${tweetQuote}`} rel="noopener noreferrer">
-            <img src={twitterLogo} className="App-logo" alt="Twitter logo" width="50" height="50" title="Share quote on Twitter" />
+            <img src={twitterLogo} className="tweet-quote-img" alt="Twitter logo" width="50" height="50" title="Share quote on Twitter" />
         </a>
         
     );
